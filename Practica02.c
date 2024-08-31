4e3039f0de41cf10b974027c8ad94fe458bfd948
Dani-Mol <78124826+Dani-Mol@users.noreply.github.com>
2024-08-30 23:57:47 -0600

    Dos funciones mas

--- Practica02.c
+++ Practica02.c
@@ -5,28 +5,30 @@
 
 int numArgs;
 
-float media_aritmetica(float datos [], int n){
 
+float media_aritmetica(float datos [], int n){
+    float suma = 0;
+    for (int i = 0; i < n; i++){
+        suma += datos[i];
+    }
+    return suma/n;
 }
 
 float media_armonica(float datos [], int n){
+    
 
 } 
 
 float media_geometrica(float datos [], int n){
-
-}
-
-int promedio(){
-
-
+    float multi = 0;
+    for (int i = 0; i < n; i++){
+        multi *= datos[i];
+    }
+    return sqrt(multi);
 }
 
 int main(int argc, char* argv[]){
     numArgs = argc - 2; 
-
-
-
 }
 
 

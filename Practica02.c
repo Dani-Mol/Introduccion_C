5611904494c648d4202743897af2158af28ee807
Dani-Mol <78124826+Dani-Mol@users.noreply.github.com>
2024-08-30 18:36:43 -0600

    Esqueletos de funciones

---
+++ Practica02.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+int numArgs;
+
+float media_aritmetica(float datos [], int n){
+
+}
+
+float media_armonica(float datos [], int n){
+
+} 
+
+float media_geometrica(float datos [], int n){
+
+}
+
+int promedio(){
+
+
+}
+
+int main(int argc, char* argv[]){
+    numArgs = argc - 2; 
+
+
+
+}
+
+

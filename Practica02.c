3b9c0c6dd309600a778e35244af46efa483b2b18
Dani-Mol <78124826+Dani-Mol@users.noreply.github.com>
2024-09-02 14:13:38 -0600

    Funciones

--- Practica02.c
+++ Practica02.c
@@ -4,10 +4,10 @@
 #include <string.h>
 
 int numArgs;
-
+float suma, multi;
 
 float media_aritmetica(float datos [], int n){
-    float suma = 0;
+    suma = 0.0;
     for (int i = 0; i < n; i++){
         suma += datos[i];
     }
@@ -15,20 +15,57 @@ float media_aritmetica(float datos [], int n){
 }
 
 float media_armonica(float datos [], int n){
+    suma = 0.0;
+    for (int i = 0; i < n; i++){
+        suma += 1.0/datos[i];
+    }
+    return n/suma;
     
-
 } 
 
 float media_geometrica(float datos [], int n){
-    float multi = 0;
+    multi = 1.0;
     for (int i = 0; i < n; i++){
         multi *= datos[i];
     }
-    return sqrt(multi);
+    return pow(multi, 1.0/n);
 }
 
 int main(int argc, char* argv[]){
-    numArgs = argc - 2; 
+    if (argc < 3) {
+        printf("Uso: %s <A|H|G> <lista de números>\n", argv[0]);
+        return 1;
+    }
+
+    char opcion = argv[1][0];
+    int n = argc - 2;  // Número de datos de entrada
+    float datos[n];
+
+    for (int i = 0; i < n; i++) {
+        datos[i] = atof(argv[i + 2]);
+    }
+
+    float resultado = 0.0;
+
+    switch (opcion) {
+        case 'A':
+            resultado = media_aritmetica(datos, n);
+            break;
+        case 'H':
+            resultado = media_armonica(datos, n);
+            break;
+        case 'G':
+            resultado = media_geometrica(datos, n);
+            break;
+        default:
+            printf("Opción no válida. Use 'A' para media aritmética, 'H' para media armónica, o 'G' para media geométrica.\n");
+            return 1;
+    }
+
+    printf("El resultado de la media es: %.2f\n", resultado);
+
+    return 0;
+
 }
 
 
